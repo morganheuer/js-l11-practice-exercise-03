6ae3b89dca35f786e376cd330781fa90e6497048
morganheuer <morganheuer@gmail.com>
2021-07-07 13:20:50 -0700

    Created list of unregistered students

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const notRegistered = document.querySelector(".not-registered");
+
+const getRegistrationData = async function(){
+    const request = await fetch(`https://gist.githubusercontent.com/skillcrush-curriculum/4bfa96d3762f86d2327d389063b3ea8d/raw/86e50e48e98c662ead6d9bd191ef7db381e9f4b4/fieldtrip.json`);
+    const data = await request.json();
+    console.log(data);
+    displayContactList(data);
+};
+
+const displayContactList = function(data) {
+    for (const student of data) {
+        const registrationStatus = student.registered;
+        const name = student.name;
+        if (registrationStatus === "no") {
+            const li = document.createElement("li");
+            li.innerHTML = `
+                    <h3>${name}</h3>
+                `;
+            notRegistered.append(li);
+        }
+    }
+};
+
+getRegistrationData();
